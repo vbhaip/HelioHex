7bf2182193acf5c5564d70b65a0e228ec038f18e
vbhaip <vbhaip01@gmail.com>
2020-04-27 23:29:56 -0400

    Basic working controller that makes http requests to the endpoints on the raspberry pi

--- heroku-app/app.py
+++ heroku-app/app.py
@@ -1,7 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+import urllib.request
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def index():
-	return "<h1>Made it to landing page</h1>"
+	return render_template('index.html') 
+
+@app.route("/ping_spotify")
+def ping_spotify():
+	urllib.request.urlopen(os.environ['RPI_BASE_URL'] + "play_song")
+	return (''), 204
+
+@app.route("/flash_around")
+def flash_around():
+	urllib.request.urlopen(os.environ['RPI_BASE_URL'] + "flash_around")
+	return (''), 204
+
+@app.route("/rainbow_wheel")
+def rainbow_wheel():
+	urllib.request.urlopen(os.environ['RPI_BASE_URL'] + "rainbow_cycle")
+	return (''), 204
+
+@app.route("/set_color")
+def set_color():
+	urllib.request.urlopen(os.environ['RPI_BASE_URL'] + "set_color/200.200.0")
+	return (''), 204
+
+app.jinja_env.globals.update(ping_spotify=ping_spotify, flash_around=flash_around, rainbow_wheel=rainbow_wheel, set_color=set_color)
+
+def main():
+	app.run(host="0.0.0.0", port=5000)
+
+if __name__ == "__main__":
+	main()
---
+++ heroku-app/static/index.js
@@ -0,0 +1,35 @@
+//https://stackoverflow.com/questions/21566649/flask-button-run-python-without-refreshing-page
+// Only run what comes next *after* the page has loaded
+addEventListener("DOMContentLoaded", function() {
+
+  var buttons = document.querySelectorAll(".button");
+  for (var i=0, l=buttons.length; i<l; i++) {
+    var button = buttons[i];
+    // For each button, listen for the "click" event
+    button.addEventListener("click", function(e) {
+      // When a click happens, stop the button
+      // from submitting our form (if we have one)
+      e.preventDefault();
+
+      var clickedButton = e.target;
+      var command = clickedButton.value;
+
+      // Now we need to send the data to our server
+      // without reloading the page - this is the domain of
+      // AJAX (Asynchronous JavaScript And XML)
+      // We will create a new request object
+      // and set up a handler for the response
+      var request = new XMLHttpRequest();
+      request.onload = function() {
+          // We could do more interesting things with the response
+          // or, we could ignore it entirely
+          //alert(request.responseText);
+		  console.log(request.responseText);
+      };
+      // We point the request at the appropriate command
+      request.open("GET", command, true);
+      // and then we send it off
+      request.send();
+    });
+  }
+}, true);
